58f33343f10208fc148c4d919bf3db58e7826a68
niwo <nik.wolfgramm@gmail.com>
2017-05-09 18:01:58 +0200

    set account/project directly in measures name

--- lib/cloudstack_stats/feed.rb
+++ lib/cloudstack_stats/feed.rb
@@ -57,9 +57,9 @@ module CloudstackStats
     def stat_to_line(obj, type)
       fields = CloudstackStats::CS_STATS.map {|name| "#{name}=#{obj[name] || 0}i" }
       tags = CloudstackStats::CS_TAGS.map {|name| "#{name}=#{obj[name]}" }
+      "#{type}." +
       obj["name"].downcase.tr(" ", "-").tr("--", "-").gsub(/[^0-9A-Za-z-_]/, '') +
         "," + tags.join(",") +
-        ",type=#{type}" +
         " " + fields.join(",")
     end
 
